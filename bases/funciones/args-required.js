76a0d87cac61a4c2d6c80c11afe914505caa93f2
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-23 17:33:02 -0600

    Parámetros obligatorios de las funciones

---
+++ bases/funciones/args-required.js
@@ -0,0 +1,11 @@
+(() => {
+    const fullName = (firstName, lastName) => {
+        return `${firstName} ${lastName}`;
+    };
+    // let noName: any;
+    // const name = fullName(noName, 'Stark');
+    const name = fullName('Tony', 'Stark');
+    console.log({ name });
+})();
+export {};
+//# sourceMappingURL=args-required.js.map
