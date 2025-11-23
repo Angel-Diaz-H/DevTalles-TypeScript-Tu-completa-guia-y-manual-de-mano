b0c10e6383bd4ce3b1fc93aaa23f68ff63c50779
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-23 14:44:04 -0600

    Tipo Any

---
+++ bases/tipos/any.js
@@ -0,0 +1,15 @@
+(() => {
+    let avenger = 123;
+    let exists;
+    let power;
+    avenger = 'Dr Strange';
+    // console.log(avenger.charAt(0));
+    console.log(avenger.charAt(0));
+    avenger = 15.3234324234;
+    console.log(avenger.toFixed(2));
+    console.log(avenger.toFixed(0));
+    console.log(exists);
+    console.log(power);
+})();
+export {};
+//# sourceMappingURL=any.js.map
