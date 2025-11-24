71dfedc1857c17c6410abe8f7c843d8c3f065cd1
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-23 23:06:41 -0600

    Tarea y resolución del ejercicio práctivo #2

--- bases/app.js
+++ bases/app.js
@@ -1,36 +1,27 @@
-(() => {
-    // Tipos
-    const batman = 'Bruce';
-    const superman = 'Clark';
-    const existe = false;
-    // Tuplas
-    const parejaHeroes = [batman, superman];
-    const villano = ['Lex Lutor', 5, true];
-    // Arreglos
-    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
-    //Enumeraciones
-    let fuerzaHeroes;
-    (function (fuerzaHeroes) {
-        fuerzaHeroes[fuerzaHeroes["aquaman"] = 0] = "aquaman";
-        fuerzaHeroes[fuerzaHeroes["batman"] = 1] = "batman";
-        fuerzaHeroes[fuerzaHeroes["flash"] = 5] = "flash";
-        fuerzaHeroes[fuerzaHeroes["superman"] = 100] = "superman";
-    })(fuerzaHeroes || (fuerzaHeroes = {}));
-    const fuerzaFlash = fuerzaHeroes.flash;
-    const fuerzaSuperman = fuerzaHeroes.superman;
-    const fuerzaBatman = fuerzaHeroes.batman;
-    const fuerzaAcuaman = fuerzaHeroes.aquaman;
-    // Retorno de funciones
-    function activar_batiseñal() {
-        return 'activada';
+// Funciones Básicas
+function sumar(a, b) {
+    return a + b;
+}
+const contar = (heroes) => {
+    return heroes.length;
+};
+const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+contar(superHeroes);
+//Parametros por defecto
+const llamarBatman = (llamar = true) => {
+    if (llamar) {
+        console.log("Batiseñal activada");
     }
-    function pedir_ayuda() {
-        console.log('Auxilio!!!');
-    }
-    // Aserciones de Tipo
-    const poder = '100';
-    const largoDelPoder = poder.length;
-    console.log(largoDelPoder);
-})();
+};
+llamarBatman();
+// Rest?
+const unirheroes = (...personas) => {
+    return personas.join(", ");
+};
+// Tipo funcion
+const noHaceNada = (numero, texto, booleano, arreglo) => { };
+// Crear el tipo de funcion que acepte la funcion "noHaceNada"
+let noHaceNadaTampoco;
+noHaceNadaTampoco = noHaceNada;
 export {};
 //# sourceMappingURL=app.js.map
