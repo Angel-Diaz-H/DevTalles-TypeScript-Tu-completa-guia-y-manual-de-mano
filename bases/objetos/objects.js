31a350b77adde5dcd91edfb782280d37586fec38
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-27 13:26:02 -0600

    ¿Cómo crear objetos con tipos específicos?

---
+++ bases/objetos/objects.js
@@ -0,0 +1,19 @@
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super velocidad', 'Viajar en el tiempo'],
+    };
+    flash = {
+        name: 'Clark Kent',
+        // age: 60,
+        powers: ['Super fuerza'],
+        // No es permitido 
+        getNombre() {
+            return this.name;
+        }
+    };
+    console.log(flash);
+})();
+export {};
+//# sourceMappingURL=objects.js.map
