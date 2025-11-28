e1c8e332e298a04f253f9c0c7d5103abda7e2079
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-28 15:43:02 -0600

    Métodos públicos y privados

--- bases/classes/basica.ts
+++ bases/classes/basica.ts
@@ -1,14 +1,30 @@
 (() => {
     class Avenger {
         static avgAge: number = 35;
+        static getAvgAge(){
+            return this.name;
+        }
 
         constructor(
             private name: string,
             private team: string,
             public realName?: string,
         ) { }
+
+
+        public bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
 
-    const antman: Avenger = new Avenger("Antman", "Avengers", "Scott Lang");
+
+    const antman: Avenger = new Avenger("Antman", "Capitan", "Scott Lang");
     console.log(antman);
+    
+    console.log(Avenger.getAvgAge());
+
+    // Aunque es privado, se puede acceder dentro de la clase en ES6.
+    // En ESNext no se puede.
+    // console.log(antman.bio());
+
 })();
