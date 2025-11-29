c2a18fb0644f8dbfb2473b6d6c258f88abedafb1
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-29 00:29:48 -0600

    Tarea - Resolver errores en TypeScript

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,10 +1,8 @@
-// import { Hero as SuperHero, Hero2 } from "./classes/Hero";
-import * as HeroClasses from "./classes/Hero";
-import powers, {Power} from "./data/powers";
+import { Hero } from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero";
+// import powers, {Power} from "./data/powers";
 
-const Hero = 123;
+const ironman = new Hero('Ironman', 1, 45);
 
-const ironman = new HeroClasses.Hero('Ironman', 1, 45);
 console.log(ironman);
-
-console.log(powers);
+console.log(ironman.power);
