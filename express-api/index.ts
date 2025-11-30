1350d623cbec16304b94ded517e196e868c83a90
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-29 21:25:41 -0600

    Creando un Rest API con Express

--- express-api/index.ts
+++ express-api/index.ts
@@ -3,12 +3,12 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.json({
-    ok: true,
-    msg: 'Todo salió bien!'
-  })
+    res.json({
+        ok: true,
+        msg: 'Todo salió bien!'
+    })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
